047df55917cdd3c55e4da312216f7c3611ea9b8d
Kristina Faye <kglinoga@gmail.com>
2022-11-16 14:49:56 -0800

    WOW what a journey. Styled a fairbnb logo from a react icon and text, which was fun, but burned TOO much time. now using a screenshot of the scrimba logo and turns out you put an images folder in the public folder and you don't call the public one. strange

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <h2>it's the App component y'all!</h2>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import Logo from "./public/images/logo.png"
+
+export default function Navbar() {
+    return (
+        <navbar className="navbar">
+            <img src="./images/logo.png" alt="airbnb logo" />
+        </navbar>
+    )
+}
