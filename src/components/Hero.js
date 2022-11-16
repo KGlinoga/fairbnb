155733e4fffd3553dd5c8693e13a63dbffe7a42a
Kristina Faye <kglinoga@gmail.com>
2022-11-16 15:00:33 -0800

    static portion done

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="./images/hero.png" alt="airbnb collage" />
+            <h2>Online Experiences</h2>
+            <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
+        </section>
+    )
+}
