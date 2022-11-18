70d0812232d483eb789b92ba1e09c2043a2ba7d3
Kristina Faye <kglinoga@gmail.com>
2022-11-17 16:02:08 -0800

    changing hero pic for deployment

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src="./images/webapp.png" alt="airbnb collage" />
+            <img src="./images/hero.png" alt="airbnb collage" />
             <h2>Online Experiences</h2>
             <p>Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
         </section>
